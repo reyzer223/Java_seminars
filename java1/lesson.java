085e5400bb6ca9cf6dd65d628aa4c75b578011ff
Reyzer223 <reyzerlg@inbox.ru>
2023-09-01 22:31:57 +0600

    add java liv

--- java1/lesson.java
+++ java1/lesson.java
@@ -1,9 +1,21 @@
+// package java1;
+// /**
+//  * 1 lesson
+//  */
+// public class lesson {
+//     public static void main(String[] args) {
+//         System.out.println("world");
+//     }
+// }
+
 package java1;
+
 /**
- * 1 lesson
+ * lesson
  */
 public class lesson {
     public static void main(String[] args) {
-        System.out.println("world");
+        lib.sayhi();
     }
+    
 }
---
+++ java1/lib.java
@@ -0,0 +1,6 @@
+package java1;
+public class lib {
+    static void sayhi(){
+    System.out.println("hi");
+    }
+}
